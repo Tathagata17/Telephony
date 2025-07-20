c146cf1c3c044d2963cca9ee1811d40a3352da92
tathagata17 <tathagata14n14@gmail.com>
2025-07-20 21:20:56 +0530

    Initial commmit

---
+++ src/test/java/com/telephony/AuthService/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telephony.AuthService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
